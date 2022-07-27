0e1dca31ee764cbe3c00a743ff0ebc3856f52228
Thomas Peuß <thomas@peuss.de>
2022-07-27 19:34:56 +0200

    Make avrdude work

--- src/main.c
+++ src/main.c
@@ -2,11 +2,11 @@
 #include <util/delay.h>
 
 int main(void) {
-    DDRB |= (1 << DDB1);
+    DDRB |= (1 << DDB5);
     while (1) {
-        PORTB |= (1 << PORTB1);
+        PORTB |= (1 << PORTB5);
         _delay_ms(1000);
-        PORTB &= ~(1 << PORTB1);
+        PORTB &= ~(1 << PORTB5);
         _delay_ms(1000);
     }
 }
