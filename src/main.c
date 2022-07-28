7b89ab97096739f92c3cb8e462faaf13d55dc185
Thomas Peuß <thomas@peuss.de>
2022-07-28 18:25:22 +0200

    make it more dynamic

--- src/main.c
+++ src/main.c
@@ -4,45 +4,53 @@
 
 #define DELAY 4
 volatile uint8_t brightness;
+volatile uint8_t activeLed;
+uint16_t rand = 0x1234;
 
-static inline void initTimer0(void) {
+static inline uint16_t nextRand(void) {
+    return rand = 2053 * rand + 13849;
+}
+
+static inline void startTimer0(void) {
     TCCR0B |= (1 << CS01) | (1 << CS00); // divide by 64
-   // TIMSK0 |= ((1 << OCIE0A) | (1 << OCIE1B));
-    TIMSK0 |= ((1 << OCIE0A) );
-    TIMSK0 |= (1 << TOIE0);
-    sei();
+    // TIMSK0 |= ((1 << OCIE0A) | (1 << OCIE1B));
+    TIMSK0 |= ((1 << OCIE0A) | (1 << TOIE0));
+}
+
+static inline void stopTimer0(void) {
+    TIMSK0 &= ~((1 << OCIE0A) | (1 << TOIE0));
 }
 
 ISR(TIMER0_OVF_vect) {
-    PORTB |= (1 << PORTB5);
+    PORTB |= (1 << activeLed);
     OCR0A = brightness;
 }
 
 ISR(TIMER0_COMPA_vect) {
-    PORTB &= ~ (1 << PORTB5);
+    PORTB &= ~(1 << activeLed);
 }
 
 int main(void) {
-    DDRB |= (1 << DDB5);
-    initTimer0();
-    uint16_t rand=0x1234;
+    activeLed = PB5;
+    DDRB |= (1 << activeLed);
+    sei();
+
+    brightness = 0;
     while (1) {
-        for (uint8_t i=0;i<255;++i) {
+        startTimer0();
+        for (uint8_t i = 0; i <= 255; ++i) {
             _delay_ms(DELAY);
-            brightness=i;
+            brightness = i;
         }
-        for (uint8_t i=255;i>0;--i) {
+        for (uint8_t i = 255; i >= 0; --i) {
             _delay_ms(DELAY);
-            brightness=i;
+            brightness = i;
         }
-        rand = 2053 * rand + 13849;
-        uint8_t delayCount = rand & 0xf;
-        for (uint8_t i =0; i< delayCount; ++i) {
+
+        uint8_t delayCount = nextRand() & 0xf;
+        for (uint8_t i = 0; i < delayCount; ++i) {
             _delay_ms(200);
         }
-/*        PORTB |= (1 << PORTB5);
-        _delay_ms(1000);
-        PORTB &= ~(1 << PORTB5);
-        _delay_ms(1000);*/
+        stopTimer0();
     }
 }
